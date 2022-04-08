d34652b9739fc4045de8a125769a2a98f69e07e1
Mikulas Sychra <mikulas.sychra@lolo.zone>
2022-04-08 16:49:25 +0200

    added devproxy to resolve cors

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -3,4 +3,7 @@ let publicPath = process.env.NODE_ENV === 'production' ? 'light-blue-vue-admin/'
 module.exports = {
   publicPath,
   productionSourceMap: false,
+  devServer: {
+    proxy: 'https://rtm.thinx.cloud/api',
+  }
 };
