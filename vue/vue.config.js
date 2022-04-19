f7e28f4de35b976bbce32fd2bf6ff239b38e6b22
Mikulas Sychra <mikulas.sychra@lolo.zone>
2022-04-19 12:05:49 +0200

    extending dev linter

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -2,6 +2,7 @@ let publicPath = process.env.NODE_ENV === 'production' ? '/' : '/';
 
 module.exports = {
   publicPath,
+  lintOnSave: process.env.NODE_ENV !== 'production',
   productionSourceMap: false,
   devServer: {
     proxy: 'https://console.thinx.cloud/api',
