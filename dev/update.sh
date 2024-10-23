e0bff4e546ee2e9de7b9bbfe37097be69d1f9de4
Matej Sychra <suculent@me.com>
2024-10-23 15:43:34 +0200

    updated to node 21

--- dev/update.sh
+++ dev/update.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build -t thinxcloud/console-build-env . && docker push thinxcloud/console-build-env
+# --platform linux/arm64/v8,linux/amd64
+
+docker buildx build --push \
+--platform linux/amd64 \
+-t thinxcloud/console-build-env .
